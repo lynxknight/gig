c6ca1e04c333fc6b3a91980579ebd7d232d6d963
lynxknight <decimal.counter@gmail.com>
2018-02-27 21:37:20 +0300

    manual refs retrieval

--- gitutil.go
+++ gitutil.go
@@ -2,37 +2,112 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"os/exec"
-	"strconv"
+	"path"
+	"path/filepath"
 	"strings"
+	"time"
 )
+
+type ref struct {
+	name  string
+	mdate time.Time
+}
+
+type costcacheT map[string]int
 
 type branch struct {
 	name      string
-	costcache map[string]int
+	costcache costcacheT
+}
+
+func isGitRoot(dir string) bool {
+	direntries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatalln("Failed to read directory", dir, err)
+	}
+	for _, d := range direntries {
+		if d.Name() == ".git" {
+			return true
+		}
+		// ReadDir is sorted, "." should be first, if not => we lost
+		if !strings.HasPrefix(d.Name(), ".") {
+			return false
+		}
+	}
+	return false
+}
+
+func getGitRoot() string {
+	// get cwd
+	// move up until you notice .git folder
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Failed to get cwd:", err)
+	}
+	for !isGitRoot(cwd) && cwd != "/" {
+		cwd = path.Dir(cwd)
+	}
+	if cwd == "/" && !isGitRoot(cwd) {
+		log.Fatalln("fatal: not a git repository (or any of the parent directories): .git")
+	}
+	return path.Join(cwd, ".git")
+}
+
+func getRefsRec(prefix, dir string) []ref {
+	s := make([]ref, 0)
+	direntries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatalln("Failed to read directory", dir, err)
+	}
+	newPrefix := path.Join(prefix, dir)
+	for _, d := range direntries {
+		if d.IsDir() {
+			s = append(
+				s, getRefsRec(newPrefix, path.Join(newPrefix, d.Name()))...,
+			)
+			continue
+		}
+		s = append(
+			s, ref{name: path.Join(newPrefix, d.Name()), mdate: d.ModTime()},
+		)
+	}
+	return s
+}
+
+func getRefs(gitRoot string) []ref {
+	refsPath := path.Join(gitRoot, "refs", "heads")
+	return getRefsRec("", refsPath)
 }
 
 func getBranches() []branch {
-	gbOutput, err := exec.Command(
-		"git", "for-each-ref",
-		"--format", "%(refname:short)|%(creatordate:unix)",
-		"refs/heads/",
-	).Output()
-	if err != nil {
-		panic(err)
-	}
-	rawBranches := strings.Split(string(gbOutput), "\n")
-	// Last line contains emptyspace, strip it via -1 in make() and for{}
-	branches := make([]branch, len(rawBranches)-1)
-	for i := 0; i < len(rawBranches)-1; i++ {
-		splitted := strings.Split(rawBranches[i], "|")
-		timestamp, _ := strconv.Atoi(splitted[1])
-		costcache := make(map[string]int)
-		costcache[""] = -timestamp // Empty querystring = sort by date
-		branches[i] = branch{
-			name:      splitted[0],
-			costcache: costcache,
+	// git for-each-ref is unstable across git versions, so we implement it
+	gitRoot := getGitRoot()
+	refs := make([]ref, 0)
+	refsPath := path.Join(gitRoot, "refs", "heads")
+	// TODO: refs/ provides full path, so we could not checkout into this
+	// refs := getRefs(gitRoot)
+	filepath.Walk(refsPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatalln("Runtime failed to walk on path", path, err)
 		}
+		if !info.IsDir() {
+			refs = append(refs, ref{name: info.Name(), mdate: info.ModTime()})
+		}
+		return nil
+	})
+	branches := make([]branch, len(refs))
+	for i := range refs {
+		branches[i] = branch{
+			name:      refs[i].name,
+			costcache: make(costcacheT),
+		}
+		// Empty QS = sort by date
+		// Screw you if you run on 32bit system
+		branches[i].costcache[""] = -int(refs[i].mdate.Unix())
 	}
 	return branches
 }
