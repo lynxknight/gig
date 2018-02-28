2754cfa7775d7aa635193ffb4a11e0c5b1f2e85a
lynxknight <decimal.counter@gmail.com>
2018-02-28 23:38:49 +0300

    analyze packed-refs

--- gitutil.go
+++ gitutil.go
@@ -57,22 +57,51 @@ func getGitRoot() string {
 	return path.Join(cwd, ".git")
 }
 
+func getPackedRefs(gitRoot string) ([]ref, error) {
+	// git has a "more perfomant" way of storing refs info, called "packed-refs"
+	// more info at https://git-scm.com/docs/git-pack-refs
+	byteContent, err := ioutil.ReadFile(path.Join(gitRoot, "packed-refs"))
+	if err != nil {
+		return nil, err
+	}
+	content := string(byteContent)
+	refsHeads := "refs/heads" // we care about heads only now
+	refs := make([]ref, 0)
+	for _, line := range strings.Split(content, "\n") {
+		objects := strings.Split(line, " ")
+		if len(objects) != 2 { // if it is a ^SHA record...
+			continue // it is not interesting to us
+		}
+		refName := objects[1]
+		if strings.HasPrefix(refName, refsHeads) {
+			refs = append(
+				refs,
+				ref{name: refName[len(refsHeads)+1:], mdate: time.Unix(0, 0)},
+			)
+		}
+	}
+	return refs, nil
+}
+
 func getBranches() []branch {
 	// git for-each-ref is unstable across git versions, so we implement it
 	gitRoot := getGitRoot()
 	refs := make([]ref, 0)
 	refsPath := path.Join(gitRoot, "refs", "heads")
-	// TODO: refs/ provides full path, so we could not checkout into this
-	// refs := getRefs(gitRoot)
 	filepath.Walk(refsPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Fatalln("Runtime failed to walk on path", path, err)
 		}
 		if !info.IsDir() {
+			// TODO: what if ref is presented in both refs and packed-refs?
 			refs = append(refs, ref{name: path[len(refsPath)+1:], mdate: info.ModTime()})
 		}
 		return nil
 	})
+	packedRefs, err := getPackedRefs(gitRoot)
+	if err == nil {
+		refs = append(refs, packedRefs...)
+	}
 	branches := make([]branch, len(refs))
 	for i := range refs {
 		branches[i] = branch{
